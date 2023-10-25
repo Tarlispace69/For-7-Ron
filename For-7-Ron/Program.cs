1a67cd0ca88c5185cf4cd503ebc9d1dd04f4ff52
Tarlispace69 <116681079+Tarlispace69@users.noreply.github.com>
2023-10-25 13:24:38 +0300

    For-7-Ron complete

---
+++ For-7-Ron/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace For_7_Ron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0, i = 0, sum = 0;
+
+            for(i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Enter a number:");
+                input = int.Parse(Console.ReadLine());
+
+                if (input / 10 != 0 && input % 2 == 0)
+                {
+                    sum += input;
+                }
+            }
+            Console.WriteLine($"The sum of even dual-digit numbers is: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
